e37c009056d7cd7287d14d42a8abf4b1ac1c6be7
DESKTOP-TGDIJBV\JION KIM <3161508@naver.com>
2024-05-24 17:23:00 +0900

    Add z-concatenation module

--- tools/dataloader.py
+++ tools/dataloader.py
@@ -161,8 +161,6 @@ class Image3DDatasetCond(Dataset):
         self.size = len(self.indices)
         print(self.size)
         self.shuffle_indices = self.indices
-        # self.shuffle_indices = [i for i in range(self.size)]
-        # random.shuffle(self.shuffle_indices)
 
         self._need_init = True
 
@@ -190,8 +188,6 @@ class Image3DDatasetCond(Dataset):
         img_paths = self.image_3d_list[img_idx]
         slice_nums = self.slices_num_list[img_idx]
 
-        # print(img_paths)
-
         # 3D image load
         img_datas = []
         for img_path in img_paths:
@@ -203,6 +199,133 @@ class Image3DDatasetCond(Dataset):
 
         return img_datas, slice_nums, idx
 
+class Image3DDatasetCondMask(Dataset):
+    def __init__(self,
+                 root,
+                 root_mask,
+                 train,
+                 resolution,
+                 n_frames=16,
+                 skip=1,
+                 fold=1,
+                 use_labels=False,  # Enable conditioning labels? False = label dimension is zero.
+                 return_vid=False,  # True for evaluating FVD
+                 seed=42,
+                 ):
+        image_3d_root = osp.join(os.path.join(root))
+        image_3d_root_mask = osp.join(os.path.join(root_mask))
+        if not 1 <= fold <= 3:
+            raise ValueError("fold should be between 1 and 3, got {}".format(fold))
+
+        self.path = image_3d_root
+        self.path_mask = image_3d_root_mask
+        name = image_3d_root.split('/')[-1]
+        self.name = name
+        self.train = train
+        self.fold = fold
+        self.resolution = resolution
+        self.nframes = n_frames
+        self.classes = list(natsorted(p for p in os.listdir(image_3d_root) if osp.isdir(osp.join(image_3d_root, p))))
+        class_to_idx = {self.classes[i]: i for i in range(len(self.classes))}
+        self.samples = make_dataset(image_3d_root, class_to_idx, ('nii.gz',), is_valid_file=None)
+        self.samples_mask = make_dataset(image_3d_root_mask, class_to_idx, ('nii.gz',), is_valid_file=None)
+
+        image_3d_list = [x[0] for x in self.samples]
+        image_3d_list_mask = [x[0] for x in self.samples_mask]
+        vol_num_list = [int(os.path.basename(x[0]).split('_')[0]) for x in self.samples] # Extract volume number from path
+        vol_num_list_unique = list(set(vol_num_list))
+        slices_num_list = [x[1] for x in self.samples]
+
+        # Bind image_3d_list and slice_num_list with same data
+        image_3d_list_c = []
+        image_3d_list_mask_c = []
+        slices_num_list_c = []
+        for vn_l in vol_num_list_unique:
+            img_3d_list = []
+            img_m_3d_list = []
+            sls_num_list = []
+            for idx, val in enumerate(vol_num_list):
+                if val == vn_l:
+                    img_3d_list.append(image_3d_list[idx])
+                    img_m_3d_list.append(image_3d_list_mask[idx])
+                    sls_num_list.append(slices_num_list[idx])
+
+            image_3d_list_mask_c.append(img_m_3d_list)
+            image_3d_list_c.append(img_3d_list)
+            slices_num_list_c.append(sls_num_list)
+
+        self.image_3d_list = image_3d_list_c
+        self.image_3d_list_mask = image_3d_list_mask_c
+        self.vol_num_list = vol_num_list
+        self.slices_num_list = slices_num_list_c
+        self._use_labels = use_labels
+        self._label_shape = None
+        self._raw_labels = None
+        self._raw_shape = [len(self.image_3d_list)] + [16, resolution, resolution]
+        self.num_channels = 1
+        self.return_vid = return_vid
+
+        frames_between_clips = skip
+        print(root, frames_between_clips, n_frames)
+        if train:
+            self.indices = [i for i in range(0, int(len(self.image_3d_list) * 0.7))]
+        else:
+            self.indices = [i for i in range(int(len(self.image_3d_list) * 0.7), len(self.image_3d_list))]
+        # self.indices = self._select_fold(self.image_3d_list, self.path, fold, train)
+
+        random.seed(seed)
+
+        self.size = len(self.indices)
+        print(self.size)
+        self.shuffle_indices = self.indices
+
+        self._need_init = True
+
+    def _select_fold(self, video_list, path, fold, train):
+        name = "train" if train else "test"
+        name = "{}list{:02d}.txt".format(name, fold)
+        f = os.path.join(path, name)
+        selected_files = []
+        with open(f, "r") as fid:
+            data = fid.readlines()
+            data = [x.strip().split(" ") for x in data]
+            data = [os.path.join(self.path, x[0]) for x in data]
+            selected_files.extend(data)
+
+        selected_files = set(selected_files)
+        indices = [i for i in range(len(video_list)) if video_list[i] in selected_files]
+        return indices
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        img_idx = self.indices[idx]
+
+        img_paths = self.image_3d_list[img_idx]
+        img_paths_mask = self.image_3d_list_mask[img_idx]
+        slice_nums = self.slices_num_list[img_idx]
+
+        # 3D image load
+        img_datas = []
+        img_datas_mask = []
+
+        for idx in range (0, img_paths.__len__()):
+            img = nib.load(img_paths[idx])
+            img_data = img.get_fdata()
+            img_data = img_data.swapaxes(0, 2)
+            img_data = np.expand_dims(img_data, axis=1)
+
+            img_mask = nib.load(img_paths_mask[idx])
+            img_data_mask = img_mask.get_fdata()
+            img_data_mask = img_data_mask.swapaxes(0, 2)
+            img_data_mask = np.expand_dims(img_data_mask, axis=1)
+
+            img_datas.append(img_data)
+            img_datas_mask.append(img_data_mask)
+
+        return img_datas, img_datas_mask, slice_nums, idx
+
 def get_loaders(rank, imgstr, resolution, timesteps, skip, batch_size=1, n_gpus=1, seed=42,  cond=False):
 
     if imgstr == 'CHAOS':
@@ -211,14 +334,20 @@ def get_loaders(rank, imgstr, resolution, timesteps, skip, batch_size=1, n_gpus=
         train_dir = os.path.join(data_location, 'CHAOS_res_128_s_16_ol_0_5')
     elif imgstr == 'CHAOS_PD_2':
         train_dir = os.path.join(data_location, 'CHAOS_res_128_s_16_pd_2')
+        train_dir_mask = os.path.join(data_location, 'CHAOS_res_128_s_16_pd_2_mask')
     elif imgstr == 'CHAOS_PD_1_RES_256':
         train_dir = os.path.join(data_location, 'CHAOS_res_256_s_16_pd_1')
+    elif imgstr == 'CHAOS_PD_2_RES_256':
+        train_dir = os.path.join(data_location, 'CHAOS_res_256_s_16_pd_2')
+    elif imgstr == 'HCP_PD_2_RES_256':
+        train_dir = os.path.join(data_location, 'HCP_res_256_s_16_pd_2')
     elif imgstr == 'HCP':
         train_dir = os.path.join(data_location, 'HCP_1200_norm_res_128_s_16')
     elif imgstr == 'HCP_OL_0_5':
         train_dir = os.path.join(data_location, 'HCP_1200_norm_res_128_s_16_ol_0_5')
     elif imgstr == 'HCP_PD_2':
         train_dir = os.path.join(data_location, 'HCP_1200_norm_res_128_s_16_pd_2')
+        train_dir_mask = os.path.join(data_location, 'HCP_1200_norm_res_128_s_16_pd_2_mask')
     elif imgstr == 'CT_ORG':
         train_dir = os.path.join(data_location, 'CT-ORG_res_128_norm_s_16')
     else:
@@ -233,6 +362,12 @@ def get_loaders(rank, imgstr, resolution, timesteps, skip, batch_size=1, n_gpus=
     testset = Image3DDatasetCond(train_dir, train=False, resolution=resolution)
     print(len(testset))
 
+    '''
+    trainset = Image3DDatasetCondMask(train_dir, train_dir_mask, train=True, resolution=resolution)
+    print(len(trainset))
+    testset = Image3DDatasetCondMask(train_dir, train_dir_mask, train=False, resolution=resolution)
+    print(len(testset))
+    '''
     trainset_sampler = InfiniteSampler(dataset=trainset, rank=0, num_replicas=n_gpus, seed=seed)
     trainloader = DataLoader(trainset, sampler=trainset_sampler, batch_size=batch_size, pin_memory=False, num_workers=4, prefetch_factor=2)
     testset_sampler = InfiniteSampler(testset, num_replicas=n_gpus, rank=0, seed=seed)
---
+++ tools/trainer_cond_mask.py
@@ -0,0 +1,258 @@
+import os
+import sys
+
+sys.path.extend([sys.path[0][:-4], '/app'])
+
+import time
+import copy
+
+import torch
+from torch.cuda.amp import GradScaler, autocast
+
+from utils import AverageMeter
+from evals.eval_cond import test_psnr_mask, save_image_ddpm_cond, save_image_cond_mask
+from models.ema import LitEma
+from einops import rearrange
+
+
+def latentDDPM(rank, first_stage_model, model, opt, criterion, train_loader, test_loader, scheduler, ema_model=None,
+               cond_prob=0.3, logger=None):
+    if logger is None:
+        log_ = print
+    else:
+        log_ = logger.log
+
+    # if rank == 0:
+    rootdir = logger.logdir
+
+    device = torch.device('cuda', rank)
+
+    losses = dict()
+    losses['diffusion_loss'] = AverageMeter()
+    check = time.time()
+
+    if ema_model == None:
+        ema_model = copy.deepcopy(model)
+        ema = LitEma(ema_model)
+        ema_model.eval()
+    else:
+        ema = LitEma(ema_model)
+        ema.num_updates = torch.tensor(11200, dtype=torch.int)
+        ema_model.eval()
+
+    first_stage_model.eval()
+    model.train()
+
+    for it, (x, cond, _) in enumerate(train_loader):
+
+        it = it + 36000
+
+        # x_p_prev = rearrange(torch.zeros(x[0].shape), 'b t c h w -> b c t h w').cuda()
+        # init_context = None
+        # z_prev = None
+
+        for x_idx in range (0, x.__len__()):
+
+            x_p = x[x_idx].to(device)
+            x_p = rearrange(x_p / 127.5 - 1, 'b t c h w -> b c t h w').float()  # videos
+            cond_p = cond[x_idx].to(device)
+            # x_p_concat = torch.cat([x_p, x_p_prev], dim=1)
+
+            # conditional free guidance training
+            model.zero_grad()
+
+            with autocast():
+                with torch.no_grad():
+                    z = first_stage_model.extract(x_p, cond_p).detach()
+
+                    # if z_prev is None:
+                        # z_prev = z.clone()
+
+            # Concatenate previous latent code (z)
+            # print('train')
+            # print(z.shape)
+            # _concat = torch.cat([z, z_prev], dim=1)
+            # print(z_concat.shape)
+            # print(cond_p.shape)
+
+            # (loss, t, ic), loss_dict = criterion(z_concat.float(), cond_p.float(), init_context=None)
+            (loss, t), loss_dict = criterion(z.float(), cond_p.float())
+            # init_context = ic
+
+            loss.backward()
+            opt.step()
+
+            losses['diffusion_loss'].update(loss.item(), 1)
+
+            # x_p_prev = x_p.clone()
+            # z_prev = z.clone()
+
+        # ema model
+        if it % 5 == 0:
+            ema(model)
+
+        if it % 125 == 0:
+            # if logger is not None and rank == 0:
+            if logger is not None:
+                logger.scalar_summary('train/diffusion_loss', losses['diffusion_loss'].average, it)
+
+                log_('[Time %.3f] [Diffusion %f]' %
+                     (time.time() - check, losses['diffusion_loss'].average))
+
+                losses = dict()
+                losses['diffusion_loss'] = AverageMeter()
+
+        if it % 2000 == 0:
+            torch.save(model.state_dict(), rootdir + f'model_{it}.pth')
+            ema.copy_to(ema_model)
+            torch.save(ema_model.state_dict(), rootdir + f'ema_model_{it}.pth')
+            save_image_ddpm_cond(rank, ema_model, first_stage_model, it, logger)
+
+
+
+def first_stage_train(rank, model, opt, d_opt, criterion, train_loader, test_loader, first_model, fp, logger=None):
+    if logger is None:
+        log_ = print
+    else:
+        log_ = logger.log
+
+    # if rank == 0:
+    rootdir = logger.logdir
+
+    device = torch.device('cuda', rank)
+
+    losses = dict()
+    losses['ae_loss'] = AverageMeter()
+    losses['L1_loss'] = AverageMeter()
+    losses['d_loss'] = AverageMeter()
+    check = time.time()
+
+    accum_iter = 3
+    disc_opt = False
+
+    l1_criterion = torch.nn.L1Loss()
+
+    if fp:
+        # print('fp')
+        scaler = GradScaler()
+        scaler_d = GradScaler()
+
+        try:
+            scaler.load_state_dict(torch.load(os.path.join(first_model, 'scaler.pth')))
+            scaler_d.load_state_dict(torch.load(os.path.join(first_model, 'scaler_d.pth')))
+        except:
+            print("Fail to load scalers. Start from initial point.")
+
+    model.train()
+    disc_start = criterion.discriminator_iter_start
+
+    for it, (x, m, cond, _) in enumerate(train_loader):
+
+        it = it + 12500
+
+        # Store previous partitions
+        # x_p_prev = rearrange(torch.zeros(x[0].shape), 'b t c h w -> b c t h w').cuda()
+
+        for x_idx in range (0, x.__len__()):
+
+            batch_size = x[x_idx].size(0)
+            x_p = x[x_idx].to(device)
+            m_p = m[x_idx].to(device)
+            x_p = rearrange(x_p / 127.5 - 1, 'b t c h w -> b c t h w').float()
+            m_p = rearrange(m_p, 'b t c h w -> b c t h w').float()
+            x_p_concat = torch.cat([x_p, m_p], dim=1)
+
+            cond_p = cond[x_idx].to(device)
+
+            if not disc_opt:
+                with autocast():
+                    # x_tilde, vq_loss = model(x)
+                    x_tilde, vq_loss = model(x_p_concat, cond_p)
+                    x_tilde_ra = rearrange(x_tilde, '(b t) c h w -> b c t h w', b=batch_size)
+                    if it % accum_iter == 0:
+                        model.zero_grad()
+
+                    ae_loss = criterion(vq_loss, x_p, x_tilde_ra,
+                                        optimizer_idx=0,
+                                        global_step=it)
+                    ae_loss = ae_loss / accum_iter
+
+                    l1_loss = l1_criterion(x_p, x_tilde_ra)
+                    l1_loss = 10. * l1_loss / accum_iter
+
+                    total_loss = ae_loss
+
+                scaler.scale(total_loss).backward()
+
+                if it % accum_iter == accum_iter - 1:
+                    scaler.step(opt)
+                    scaler.update()
+
+                # print(losses)
+                losses['ae_loss'].update(ae_loss.item(), 1)
+                losses['L1_loss'].update(l1_loss.item(), 1)
+
+            else:
+                if it % accum_iter == 0:
+                    criterion.zero_grad()
+
+                with autocast():
+                    with torch.no_grad():
+                        x_tilde, vq_loss = model(x_p)
+                    d_loss = criterion(vq_loss, x_p,
+                                       rearrange(x_tilde, '(b t) c h w -> b c t h w', b=batch_size),
+                                       optimizer_idx=1,
+                                       global_step=it)
+                    d_loss = d_loss / accum_iter
+
+                scaler_d.scale(d_loss).backward()
+
+                if it % accum_iter == accum_iter - 1:
+                    # Unscales the gradients of optimizer's assigned params in-place
+                    scaler_d.unscale_(d_opt)
+
+                    # Since the gradients of optimizer's assigned params are unscaled, clips as usual:
+                    torch.nn.utils.clip_grad_norm_(criterion.discriminator_2d.parameters(), 1.0)
+                    torch.nn.utils.clip_grad_norm_(criterion.discriminator_3d.parameters(), 1.0)
+
+                    scaler_d.step(d_opt)
+                    scaler_d.update()
+
+                losses['d_loss'].update(d_loss.item() * 3, 1)
+
+            if it % accum_iter == accum_iter - 1 and it // accum_iter >= disc_start:
+                if disc_opt:
+                    disc_opt = False
+                else:
+                    disc_opt = True
+
+            # x_p_prev = x_p.detach().clone()
+
+        if it % 250 == 0:
+            psnr = test_psnr_mask(rank, model, test_loader, it, logger)
+
+            if logger is not None:
+                logger.scalar_summary('train/ae_loss', losses['ae_loss'].average, it)
+                logger.scalar_summary('train/L1_loss', losses['L1_loss'].average, it)
+                logger.scalar_summary('train/d_loss', losses['d_loss'].average, it)
+                logger.scalar_summary('test/psnr', psnr, it)
+
+                log_('[Time %.3f] [AELoss %f] [L1Loss %f] [DLoss %f] [PSNR %f]' %
+                    (time.time() - check, losses['ae_loss'].average, losses['L1_loss'].average,
+                    losses['d_loss'].average, psnr))
+
+                torch.save(model.state_dict(), rootdir + f'model_last.pth')
+                torch.save(criterion.state_dict(), rootdir + f'loss_last.pth')
+                torch.save(opt.state_dict(), rootdir + f'opt.pth')
+                torch.save(d_opt.state_dict(), rootdir + f'd_opt.pth')
+                torch.save(scaler.state_dict(), rootdir + f'scaler.pth')
+                torch.save(scaler_d.state_dict(), rootdir + f'scaler_d.pth')
+
+            losses = dict()
+            losses['ae_loss'] = AverageMeter()
+            losses['L1_loss'] = AverageMeter()
+            losses['d_loss'] = AverageMeter()
+
+        if it % 1250 == 0:
+            save_image_cond_mask(rank, model, test_loader, it, logger)
+            torch.save(model.state_dict(), rootdir + f'model_{it}.pth')
